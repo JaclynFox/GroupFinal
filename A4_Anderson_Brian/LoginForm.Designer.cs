0434d7e937713845fc90211b5295b80258196433
Jaclyn <qa6846vx@go.minnstate.edu>
2019-11-28 07:56:12 -0600

    init

---
+++ A4_Anderson_Brian/LoginForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.NewUserButton = new System.Windows.Forms.Button();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.UserNameLabel = new System.Windows.Forms.Label();
+            this.UserNameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(12, 64);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 0;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // NewUserButton
+            // 
+            this.NewUserButton.Location = new System.Drawing.Point(174, 64);
+            this.NewUserButton.Name = "NewUserButton";
+            this.NewUserButton.Size = new System.Drawing.Size(75, 23);
+            this.NewUserButton.TabIndex = 1;
+            this.NewUserButton.Text = "New User";
+            this.NewUserButton.UseVisualStyleBackColor = true;
+            this.NewUserButton.Click += new System.EventHandler(this.NewUserButton_Click);
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(93, 64);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 2;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(12, 41);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.TabIndex = 3;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // UserNameLabel
+            // 
+            this.UserNameLabel.AutoSize = true;
+            this.UserNameLabel.Location = new System.Drawing.Point(12, 15);
+            this.UserNameLabel.Name = "UserNameLabel";
+            this.UserNameLabel.Size = new System.Drawing.Size(55, 13);
+            this.UserNameLabel.TabIndex = 4;
+            this.UserNameLabel.Text = "Username";
+            // 
+            // UserNameTextBox
+            // 
+            this.UserNameTextBox.Location = new System.Drawing.Point(73, 12);
+            this.UserNameTextBox.Name = "UserNameTextBox";
+            this.UserNameTextBox.Size = new System.Drawing.Size(176, 20);
+            this.UserNameTextBox.TabIndex = 5;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(73, 38);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(176, 20);
+            this.PasswordTextBox.TabIndex = 6;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 97);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UserNameTextBox);
+            this.Controls.Add(this.UserNameLabel);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.NewUserButton);
+            this.Controls.Add(this.ExitButton);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button NewUserButton;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.Label UserNameLabel;
+        private System.Windows.Forms.TextBox UserNameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+    }
+}
+
---
+++ A4_Anderson_Brian/LoginForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace WindowsFormsApp4
+{
+    public partial class LoginForm : Form
+    {
+        bool usernameValid = false, passwordValid = false;
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            using (StreamReader sr = File.OpenText("users.txt"))
+            {
+                String s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    byte[] pepper = new byte[64];
+                    byte[] userbyte = Convert.FromBase64String(s);
+                    Array.Copy(userbyte, 0, pepper, 0, 64);
+                    var userthing = new Rfc2898DeriveBytes(UserNameTextBox.Text, pepper, 10000);
+                    byte[] userhash = userthing.GetBytes(64);
+                    int un = 1;
+                    for (int i = 0; i < 64; i++)
+                        if (userbyte[i + 64] != userhash[i])
+                            un = 0;
+                    if (un == 1)
+                    {
+                        usernameValid = true;
+                    }
+                    if (usernameValid == true)
+                    {
+                        String q = sr.ReadLine();
+                        byte[] salt = new byte[64];
+                        byte[] hashbyte = Convert.FromBase64String(q);
+                        Array.Copy(hashbyte, 0, salt, 0, 64);
+                        var passthing = new Rfc2898DeriveBytes(PasswordTextBox.Text, salt, 10000);
+                        byte[] hash = passthing.GetBytes(64);
+                        int pw = 1;
+                        for (int i = 0; i < 64; i++)
+                            if (hashbyte[i + 64] != hash[i])
+                                pw = 0;
+                        if (pw == 1)
+                        {
+                            passwordValid = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void NewUserButton_Click(object sender, EventArgs e)
+        {
+            ActiveForm = NewUserEditForm;
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
